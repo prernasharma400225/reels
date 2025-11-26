dbcfe3c423a2b924dea1f8efbf3972d61aef46a2
Prerna Sharma <prernasharma0925@gmail.com>
2025-11-26 13:49:38 +0530

    sec-commit

--- script.js
+++ script.js
@@ -1,131 +1,150 @@
 const reels = [
   {
+    isMuted : true,
     username: "iamprerna",
     likeCount: 12800,
     isLiked: false,
     commentCount: 342,
     caption: "Sunset vibes ✨🌅",
-    video: "./reels/video4.mp4",
+    video: "./reels/video11.mp4",
     userProfile: "https://i.pravatar.cc/150?img=1",
     shareCount: 120,
     isFollowed: true
   },
   {
+    isMuted : true,
     username: "techguru",
     likeCount: 45200,
     isLiked: true,
     commentCount: 980,
-    caption: "This JavaScript trick will blow your mind 🤯",
-    video: "./reels/video2.mp4",
+    caption: "travelling day😊🚉",
+    video: "./reels/video12.mp4",
     userProfile: "https://i.pravatar.cc/150?img=2",
     shareCount: 610,
     isFollowed: false
   },
   {
+    isMuted : true,
     username: "foodielife",
     likeCount: 7800,
     isLiked: false,
     commentCount: 155,
-    caption: "Best pasta recipe ever 🍝🔥",
-    video: "./reels/video3.mp4",
+    caption: "cute rabbit🐰😘",
+    video: "./reels/video13.mp4",
     userProfile: "https://i.pravatar.cc/150?img=3",
     shareCount: 90,
     isFollowed: false
   },
   {
+    isMuted : true,
     username: "travelwithme",
     likeCount: 21200,
     isLiked: true,
     commentCount: 650,
     caption: "Winter in Kashmir ❄️❤️",
-    video: "./reels/video1.mp4",
+    video: "./reels/video14.mp4",
     userProfile: "https://i.pravatar.cc/150?img=4",
     shareCount: 330,
     isFollowed: true
   },
   {
+    isMuted : true,
     username: "fitnessfreak",
     likeCount: 15800,
     isLiked: false,
     commentCount: 298,
     caption: "Leg day 🔥🏋️",
-    video: "./reels/video5.mp4",
+    video: "./reels/video15.mp4",
     userProfile: "https://i.pravatar.cc/150?img=5",
     shareCount: 200,
     isFollowed: false
   },
   {
+    isMuted : true,
     username: "dancequeen",
     likeCount: 54000,
     isLiked: true,
     commentCount: 1400,
     caption: "Try this choreography 💃🔥",
-    video: "./reels/video6.mp4",
+    video: "./reels/video16.mp4",
     userProfile: "https://i.pravatar.cc/150?img=6",
     shareCount: 890,
     isFollowed: true
   },
   {
+    isMuted : true,
     username: "petlover",
     likeCount: 9500,
     isLiked: false,
     commentCount: 230,
     caption: "My dog learns a new trick 🐶✨",
-    video: "./reels/video7.mp4",
+    video: "./reels/video17.mp4",
     userProfile: "https://i.pravatar.cc/150?img=7",
     shareCount: 110,
     isFollowed: false
   },
   {
-    username: "coderboy",
+    isMuted : true,
+    username: "💻SCS💻",
     likeCount: 36700,
     isLiked: true,
     commentCount: 720,
-    caption: "React beginners must know this! ⚛️🔥",
-    video: "./reels/video8.mp4",
+    caption: "You Can Now CHEAT in Meta Interviews.",
+    video: "./reels/video18.mp4",
     userProfile: "https://i.pravatar.cc/150?img=8",
     shareCount: 500,
     isFollowed: true
   },
   {
+    isMuted : true,
     username: "fashion_diaries",
     likeCount: 17500,
     isLiked: false,
     commentCount: 410,
     caption: "Outfit inspo for this weekend ✨👗",
-    video: "./reels/video9.mp4",
+    video: "./reels/video19.mp4",
     userProfile: "https://i.pravatar.cc/150?img=9",
     shareCount: 260,
     isFollowed: false
   },
   {
+    isMuted : true,
     username: "gamingworld",
     likeCount: 62000,
     isLiked: true,
     commentCount: 2100,
     caption: "Insane clutch moment 🎮🔥",
-    video: "./reels/video10.mp4",
+    video: "./reels/video20.mp4",
     userProfile: "https://i.pravatar.cc/150?img=10",
     shareCount: 950,
     isFollowed: true
   }
 ];
+var allReels = document.querySelector('.all-reels')
+
+var isMuted = true
 
 
-var sum = ''
-reels.forEach(function(elem){
+function addData(){
+  var sum = ''
+  reels.forEach(function(elem,idx){
     sum = sum + ` <div class="reel">
-                   <video autoplay loop muted src="${elem.video}"></video>
-                    <div class="bottom">
+    
+                   <video autoplay loop ${elem.isMuted?'muted':''} src="${elem.video}"></video>
+                   <div class="mute" id=${idx}>
+                   ${elem.isMuted?'<i class="ri-volume-mute-fill"></i>':'<i class="ri-volume-up-fill"></i>'}
+                      
+                  </div>
+                   <div class="bottom">
                         <div class="user">
                             <img src="${elem.userProfile}" alt="">
                             <h4>${elem.username}</h4>
-                            <button>${elem.isFollowed?'Unfollow':'Follow'}</button>
+                            <button id=${idx} class='follow'>${elem.isFollowed?'Unfollow':'Follow'}</button>
                         </div>
                          <h3>${elem.caption}</h3>
                     </div>
                    <div class="right">
-                    <div class="like">
+                    <div id=${idx} class="like">
                         <h4 class="like-icon icon">${elem.isLiked?'<i class="love ri-heart-3-fill"></i>':'<i class="ri-heart-3-line"></i>'}</h4>
                         <h6>${elem.likeCount}</h6>
                     </div>
@@ -146,7 +165,41 @@ reels.forEach(function(elem){
                 </div>`
 })
 
-var allReels = document.querySelector('.all-reels')
-
-
 allReels.innerHTML = sum
+}
+
+addData()
+
+allReels.addEventListener('click',function(dets){
+ if(dets.target.className == 'like'){
+   if(!reels[dets.target.id].isLiked){
+    reels[dets.target.id].likeCount++
+    reels[dets.target.id].isLiked = true
+  }else{
+    reels[dets.target.id].likeCount--
+    reels[dets.target.id].isLiked = false
+  }
+  addData()
+ }
+
+
+ if(dets.target.className == 'follow'){
+  if(!reels[dets.target.id].isFollowed){
+    reels[dets.target.id].isFollowed = true
+  }else{
+    reels[dets.target.id].isFollowed = false
+  }
+  addData()
+}
+
+
+ if(dets.target.className == 'mute'){
+  if(!reels[dets.target.id].isMuted){
+    reels[dets.target.id].isMuted = true
+  }else{
+    reels[dets.target.id].isMuted = false
+  }
+  addData()
+}
+
+})
